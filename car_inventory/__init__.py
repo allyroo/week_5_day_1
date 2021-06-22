0132c72b2815fc3853fc532483d0d28eab7ebc3a
Allyse Arucan <81602578+allyroo@users.noreply.github.com>
2021-06-21 21:38:41 -0700

    Add files via upload

---
+++ car_inventory/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import Config
+from .api.routes import api
+from .site.routes import site
+from .authentication.routes import auth
+
+app = Flask(__name__)
+
+
+app.config.from_object(Config)
